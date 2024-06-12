0b71e0d64456559b1b259ef92338243b557fbcb8
Gabriel Mayer Clary <gabrielm05.clary@gmail.com>
2024-06-12 16:50:25 -0300

    Sprint 3 "finalizada"

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Entrega sprint 3
- Entrega da sprint 3 da disciplina Trabalho Interdisciplinas - Front End da PUC Minas
+# Entrega sprint 2
+ Entrega da 2ª sprint do trabalho interdisciplinar da PUC-Minas
+
+# LER
+ A funcionalidade dos gráficos utilizando os dados do JSON na estutura da dados que selecionamos está finalizada.
---
+++ assets/appresultunico.js
@@ -0,0 +1,63 @@
+//RECEBENDO OS DADOS DO JSON SERVER
+let gastosjsFix = []
+let gastosjsVar = []
+let valorjsFix = []
+let valorjsVar = []
+async function carregarDados() {
+  const response = await fetch("./db/dbmayer.json")
+  const data = await response.json()
+  console.log(data)
+  data.mes1.gastosFix.forEach(categoria => gastosjsFix.push(categoria.categoria))
+  data.mes1.gastosVar.forEach(categoria => gastosjsVar.push(categoria.categoria))
+
+  data.mes1.gastosFix.forEach(valor => valorjsFix.push(valor.valor))
+  data.mes1.gastosVar.forEach(valor => valorjsVar.push(valor.valor))
+  
+  //CONFIGURANDO GRÁFICOS PARA RECEBER OS DADOS JSON DA PRÓXIMA SPRINT
+  const ctx = document.getElementById('pie-chart1');
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: gastosjsFix,
+      datasets: [{
+        label: 'R$',
+        data: valorjsFix,
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    }
+  });
+
+
+  const ctx2 = document.getElementById('pie-chart2');
+
+  new Chart(ctx2, {
+    type: 'pie',
+    data: {
+      labels: gastosjsFix,
+      datasets: [{
+        label: 'R$',
+        data: valorjsVar,
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    }
+  });
+
+}
+
+  
+
+//TESTES
+window.onload = carregarDados;
+console.log(gastosjsFix)
+console.log(gastosjsVar)
